12c21c0f6956cadcc4185426e54cb717539d3753
Milton Mamani <miltin.mamani010@gmail.com>
2025-06-25 20:44:36 -0300

    pedimos la frase de un personaje a la api de los simpson y lo guardamos en stateFrase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Container, Button } from "react-bootstrap";
 import Frase from "./components/Frase";
-import  logo from './assets/Logosimpson.pgn'
+import  logo from './assets/logosimpson.png'
 import { useEffect, useState } from "react";
 
 function App() {
@@ -11,18 +11,24 @@ function App() {
     obtenerFrase()
   },[]) 
 
-  const obtenerFrase = () => {
-    const respuesta = fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+  const obtenerFrase = async () => {
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
     console.log(respuesta);
+    if(respuesta.status === 200){
+      const datos = await respuesta.json()
+      console.log(datos[0])
+      // guardar en el state las frases
+      setFrase(datos[0])
+    }
   }
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo simpson" className="w-50 mb-4" />
-        <div>
+        {/* <div>
           <Spinner animation="grow" variant="info" />
-        </div> 
-        <Frase></Frase>
+        </div> */}
+        <Frase></Frase> 
         <Button variant="warning" className="mt-4">
           Enviar
         </Button>
