434dfc7f506f3bd5e98bbdcb2fa3fe08496750be
Milton Mamani <miltin.mamani010@gmail.com>
2025-06-25 20:04:42 -0300

    comenzamos a ver api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { Container, Button } from "react-bootstrap";
 import Frase from "./components/Frase";
+import  logo from './assets/Logosimpson.pgn'
+import { useEffect, useState } from "react";
 
 function App() {
-  //  https://thesimpsonsquoteapi.glitch.me/quotes
+  const [frase, setFrase] = useState({});
+  // https://thesimpsonsquoteapi.glitch.me/quotes
+
+  useEffect(()=>{
+    obtenerFrase()
+  },[]) 
+
+  const obtenerFrase = () => {
+    const respuesta = fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+    console.log(respuesta);
+  }
   return (
     <>
       <Container className="text-center my-5">
-        <img src="" alt="Logo simpson" className="w-50 mb-4" />
-
-        {/* <div>
+        <img src={logo} alt="Logo simpson" className="w-50 mb-4" />
+        <div>
           <Spinner animation="grow" variant="info" />
-        </div> */}
-
+        </div> 
         <Frase></Frase>
-
         <Button variant="warning" className="mt-4">
           Enviar
         </Button>
