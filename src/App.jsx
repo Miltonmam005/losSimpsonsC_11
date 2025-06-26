1bff888ba5835656e556fc66d78af9b81905f50b
Milton Mamani <miltin.mamani010@gmail.com>
2025-06-25 21:34:21 -0300

    agregamos mas seguridad a nuestra lojica con try y catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
-import  logo from './assets/logosimpson.png'
+import logo from "./assets/logosimpson.png";
 import { useEffect, useState } from "react";
 
 function App() {
   const [frase, setFrase] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(false);
   // https://thesimpsonsquoteapi.glitch.me/quotes
 
-  useEffect(()=>{
-    obtenerFrase()
-  },[]) 
+  useEffect(() => {
+    obtenerFrase();
+  }, []);
 
   const obtenerFrase = async () => {
-    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
-    console.log(respuesta);
-    if(respuesta.status === 200){
-      const datos = await respuesta.json()
-      console.log(datos[0])
-      // guardar en el state las frases
-      setFrase(datos[0])
+    try {
+      setMostrarSpinner(true);
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      console.log(respuesta);
+      if (respuesta.status === 200) {
+        const datos = await respuesta.json();
+        console.log(datos[0]);
+        // guardar en el state las frases
+        setFrase(datos[0]);
+        // actualizar el Spinner
+        setMostrarSpinner(false);
+      }
+    } catch (error) {
+      console.error(error);
+      //  aca puedo agrfegar un mensaje o alert que justifique que intente hacer la operacion dentro de unos minutos
     }
-  }
+  };
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo simpson" className="w-50 mb-4" />
-        {/* <div>
-          <Spinner animation="grow" variant="info" />
-        </div> */}
-        <Frase frasePops={frase}></Frase> 
-        <Button variant="warning" className="mt-4">
+
+        {mostrarSpinner ? (
+          <div className="my-4">
+            <Spinner animation="grow" variant="light" />
+          </div>
+        ) : (
+          <Frase frasePops={frase}></Frase>
+        )}
+
+        <Button variant="warning" className="mt-4" onClick={obtenerFrase}>
           Enviar
         </Button>
       </Container>
